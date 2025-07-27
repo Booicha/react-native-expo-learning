da670f6c7df3f1cab1e180519b6216bcafac2e88
awirut ariyaporn <Booicha10@gmail.com>
2025-07-27 16:40:15 +0700

    feat: set up React Navigation with Home Details and Profile screens

---
+++ pages/ProfileScreen.js
@@ -0,0 +1,17 @@
+import { View, Text, Button } from "react-native";
+
+export default function ProfileScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Awirut Ariyaporn DBT.102</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate("Home")}
+      />
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
